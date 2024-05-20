f32518f2d3928621e19c431c2a8e9ab0b90f4eca
sanjana <166891804+sanjanaitti@users.noreply.github.com>
2024-05-20 19:36:18 +0530

    Add files via upload

---
+++ webdevelopment/cal.js
@@ -0,0 +1,13 @@
+function calculateAge() {
+    const birthdate = new Date(document.getElementById('birthdate').value);
+    const today = new Date();
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const monthDiff = today.getMonth() - birthdate.getMonth();
+    const dayDiff = today.getDate() - birthdate.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    document.getElementById('result').textContent = `You are ${age} years old.`;
+}
